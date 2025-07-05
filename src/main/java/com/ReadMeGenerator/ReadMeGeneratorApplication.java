99150a2f9490b7a0a3f3a903d6f85aed9459a0c0
himanshu chandekar <himanshuchandekar946@gmail.com>
2025-07-05 16:19:04 +0530

    Initializing

---
+++ src/main/java/com/ReadMeGenerator/ReadMeGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ReadMeGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadMeGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadMeGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ReadMeGenerator/ReadMeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReadMeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadMeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
