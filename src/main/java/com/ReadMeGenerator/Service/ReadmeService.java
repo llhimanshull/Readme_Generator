0ae870159edf8ae78f500213e0fb360e3919ff5e
himanshu chandekar <himanshuchandekar946@gmail.com>
2025-09-04 03:53:07 +0530

    fixing_groq_model

--- src/main/java/com/ReadMeGenerator/Service/ReadmeService.java
+++ src/main/java/com/ReadMeGenerator/Service/ReadmeService.java
@@ -86,7 +86,7 @@ public class ReadmeService {
         conn.setDoOutput(true);
 
         String body = "{"
-                + "\"model\":\"llama3-8b-8192\","
+                + "\"model\":\"llama-3.3-70b-versatile\","
                 + "\"messages\":["
                 + "{\"role\":\"system\",\"content\":\"You are a professional README.md generator that uses emojis and markdown.\"},"
                 + "{\"role\":\"user\",\"content\":" + escapeJson(prompt) + "}"
